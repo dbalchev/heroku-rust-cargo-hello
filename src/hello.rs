a9e2898af9e9c38dc12c6d0c94d98023f9c0cdce
Alexander Zolotko <azolotko@gmail.com>
2015-04-09 08:41:33 +0200

    Fixed deprecation warning

--- src/hello.rs
+++ src/hello.rs
@@ -1,13 +1,10 @@
-#![feature(core)]
-
 extern crate iron;
 extern crate router;
 
 use std::env;
-use std::num;
 use iron::{Iron, Request, Response, IronResult};
-use router::Router;
 use iron::status;
+use router::Router;
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
@@ -29,7 +26,7 @@ static DEFAULT_PORT: Port = 8080;
 /// Look up our server port number in PORT, for compatibility with Heroku.
 fn get_server_port() -> Port {
     match env::var("PORT") {
-        Ok(val) => num::from_str_radix(&val, 10).unwrap_or(DEFAULT_PORT),
+        Ok(val) => u16::from_str_radix(&val, 10).unwrap_or(DEFAULT_PORT),
         Err(_) => DEFAULT_PORT
     }
 }
